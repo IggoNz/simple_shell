47e8c2f91be31b7bd5e4cb764cb9817a36c87b48
Iggo <Iggonzioki@gmail.com>
2023-05-14 06:26:10 -0700

    task 0

---
+++ betty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "shell.h"
+
+/**
+ * main - gives the instructions to be executed
+ * Return: if successful return 0
+ */
+int main(void)
+{
+	puts("\"C programming though difficult is fun");
+	return (0);
+}
